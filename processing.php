bf30a24084f336c682506878d42d90078a0485a8
Christopher Borchert <christopher.borchert@gmail.com>
2015-12-16 11:13:33 +0100

    Process the stresses, send to DB

--- processing.php
+++ processing.php
@@ -7,21 +7,46 @@
 // The stresses are 0 = no stress, 1 = primary, 2 = secondary
 // e.g. AARDVARK  AA1 R D V AA2 R K
 // The information we should extract the word (aardvark), and the stress structure (12)
-$handle = fopen("stresses.txt", "r");
-if ($handle) {
-    $i = 0;
-    while (($line = fgets($handle)) !== false && $i < 200) {
-        $word = strtolower(substr($line, 0, strpos($line, " "))); //get the word 
-        $word = preg_replace("/[^a-zA-Z'\.]+/", '', $word);//strip everything except letters, dashes, and apostrophes
-        $syllables = substr($line, strpos($line, " "), -1); //get the syllables
-        $syllables = preg_replace("/[^0-2]+/", '', $syllables);//strip down the syllables to just 0, 1, 2
-        echo $word." | ".$syllables."<br>";
-        $i++;
-        
-    }
-    fclose($handle);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "syllablestress";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "you f*cked up, chris";
-    // error opening the file.
+    $handle = fopen("stresses.txt", "r");
+    if ($handle) {
+        $i = 0;
+        while (($line = fgets($handle)) !== false ) {
+            $word = strtolower(substr($line, 0, strpos($line, " "))); //get the word 
+            $word = str_replace( "'", "\'", preg_replace("/[^a-zA-Z'\.]+/", '', $word));//strip everything except letters, dashes, and apostrophes
+            $stresses = substr($line, strpos($line, " "), -1); //get the stresses
+            $stresses = preg_replace("/[^0-2]+/", '', $stresses);//strip down the stresses to just 0, 1, 2
+            echo $word." | ".$stresses."<br>";
+            
+            $sql = "INSERT INTO words (word, stresses)
+            VALUES ('$word','$stresses')";
+            
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            $i++;
+        }
+        fclose($handle);
+    } else {
+        echo "you f*cked up, chris";
+        // error opening the file.
+    }
 }
+
+$conn->close();
+
 ?>
