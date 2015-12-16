fff9adb18c6278e14b4548d256485ec9bcdecd85
Christopher Borchert <christopher.borchert@gmail.com>
2015-12-16 10:26:00 +0100

    Fix stupid errors in the processing script

--- processing.php
+++ processing.php
@@ -1,14 +1,14 @@
 <?php
 $handle = fopen("stresses.txt", "r");
 if ($handle) {
-    $i == 0;
+    $i = 0;
     while (($line = fgets($handle)) !== false && $i < 10) {
         echo $line."<br>";
         $i++;
     }
     fclose($handle);
 } else {
-    echo "you f*cked up, chris"
+    echo "you f*cked up, chris";
     // error opening the file.
 }
 ?>
